8f2ab0c709256b69151094968c55bfc4d07d14a0
Tai <4382091+taisukemino@users.noreply.github.com>
2023-07-06 08:44:37 -0700

    remove the Chart component

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Card, Metric, Text, Title, BarList, Flex, Grid } from '@tremor/react';
-import Chart from './chart';
 
 const website = [
   { name: '/home', value: 1230 },
@@ -73,7 +72,6 @@ export default function PlaygroundPage() {
           </Card>
         ))}
       </Grid>
-      <Chart />
     </main>
   );
 }
