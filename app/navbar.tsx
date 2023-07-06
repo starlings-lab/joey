f4017f6c9eea53e3b620ce57599455dbaa70fb47
Tai <4382091+taisukemino@users.noreply.github.com>
2023-07-06 08:57:39 -0700

    navbar home page style change

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -4,7 +4,7 @@ import { usePathname } from 'next/navigation';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 
 const navigation = [
-  { name: 'Dashboard', href: '/' }
+  { name: 'Joey', href: '/' }
 ];
 
 function classNames(...classes: string[]) {
@@ -30,7 +30,7 @@ export default function Navbar() {
                         pathname === item.href
                           ? 'border-slate-500 text-gray-900'
                           : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                        'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                        'inline-flex items-center px-1 pt-1 text-sm font-medium'
                       )}
                       aria-current={pathname === item.href ? 'page' : undefined}
                     >
