0834c9d432b89f618672aca129ff4c3edcd698ee
vnadoda <vipul@carapace.finance>
2023-07-10 16:18:12 -0500

    Updated tvl and apy line chart to use monthly avg

--- app/components/TvlAndApyLineChart.tsx
+++ app/components/TvlAndApyLineChart.tsx
@@ -6,14 +6,39 @@ import { TvlAndApyDataPoint } from "../types";
 import numeral from "numeral";
 
 const dataFormatter = (number: number) => `${numeral(number).format('($0.00a)')}`;
+type DataByMonth = {
+  [key: string]: any;
+};
 
 export default function TvlAndApyLineChart({ historyData }: { historyData: TvlAndApyDataPoint[] }) {
-  const chartdata = historyData.map((dataPoint) => {
+
+
+  const dataByMonth = historyData.reduce((acc: DataByMonth, dataPoint) => {
+    const key = new Date(dataPoint.timestamp).toLocaleDateString('en-US', { month: 'short', year: '2-digit' });
+
+    if (!acc[key]) {
+      acc[key] = {
+        monthYear: key,
+        tvlSum: 0,
+        apySum: 0,
+        tvlCount: 0,
+        apyCount: 0
+      };
+    }
+    acc[key].tvlSum += dataPoint.tvlUsd;
+    acc[key].tvlCount++;
+    acc[key].apySum += dataPoint.apy;
+    acc[key].apyCount++;
+    return acc;
+  }, {});
+
+  // array of objects with monthYear, TVL and APY
+  const chartdata = Object.values(dataByMonth).map((dataPoint: any) => {
     return {
-      timestamp: new Date(dataPoint.timestamp).toLocaleDateString('en-US', { month: 'short', year: '2-digit' }),
-      TVL: dataPoint.tvlUsd,
-      APY: dataPoint.apy
-    };
+      monthYear: dataPoint.monthYear,
+      TVL: dataPoint.tvlSum / dataPoint.tvlCount,
+      APY: dataPoint.apySum / dataPoint.apyCount
+    }
   });
 
   return (
@@ -22,7 +47,7 @@ export default function TvlAndApyLineChart({ historyData }: { historyData: TvlAn
       <LineChart
         className="mt-6"
         data={chartdata}
-        index="timestamp"
+        index="monthYear"
         categories={["TVL"]}
         colors={["emerald", "blue"]}
         valueFormatter={dataFormatter}
