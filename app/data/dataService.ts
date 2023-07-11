90ac60f6a03c94075c61bcab2f7f72ff07698148
vnadoda <vipul@carapace.finance>
2023-07-11 17:42:47 -0500

    Moved static data from dataService to staticDataService

--- app/data/dataService.ts
+++ app/data/dataService.ts
@@ -1,69 +1,5 @@
 import { StakingProtocol, StakingProtocolDetails, StakingProtocolSummary } from '../types';
-
-// protocols that we are interested in and its DefiLlama project name/slug
-const protocolSlugs = ['lido', 'frax-ether', 'rocket-pool', 'coinbase-wrapped-staked-eth', 'stakewise'];
-const protocolMapBySlug = new Map<string, StakingProtocolSummary>();
-
-// pre-populate the map with supported protocols
-protocolSlugs.forEach((slug: string) => {
-  const stakingProtocol: StakingProtocolSummary = {
-    id: StakingProtocol.Lido,
-    name: '',
-    tvl: 0,
-    netApy: 0,
-    stakingApy: 0,
-    tokenRewardsApy: 0,
-    fees: 0,
-    logoUrl: ''
-  };
-
-  switch (slug) {
-    case 'lido':
-      stakingProtocol.id = StakingProtocol.Lido;
-      stakingProtocol.name = 'Lido';
-      stakingProtocol.logoUrl = 'https://icons.llama.fi/lido.png';
-
-      // Lido takes 10% fee on users staking rewards
-      // Source: https://docs.lido.fi/#protocol-fee
-      stakingProtocol.fees = 10;
-      break;
-    case 'frax-ether':
-      stakingProtocol.id = StakingProtocol.FraxEther;
-      stakingProtocol.name = 'Frax ETH';
-      stakingProtocol.logoUrl = 'https://icons.llama.fi/frax-ether.jpg';
-
-      // Source: https://exponential.fi/assets/b8956e66-fe6c-4a88-8e96-530e2e7dea4c
-      stakingProtocol.fees = 10;
-      break;
-    case 'rocket-pool':
-      stakingProtocol.id = StakingProtocol.RocketPool;
-      stakingProtocol.name = 'Rocket Pool';
-      stakingProtocol.logoUrl = 'https://icons.llama.fi/rocket-pool.jpg';
-
-      // TODO: how do we display 0.05% of deposit fee when ETH is depsoited through protocol?
-      // Source: https://twitter.com/Rocket_Pool/status/1506519957986758659
-      stakingProtocol.fees = 15;
-
-      break;
-    case 'coinbase-wrapped-staked-eth':
-      stakingProtocol.id = StakingProtocol.Coinbase;
-      stakingProtocol.name = 'Coinbase';
-      stakingProtocol.logoUrl = 'https://icons.llama.fi/coinbase-wrapped-staked-eth.png';
-
-      // Source: https://help.coinbase.com/en/coinbase/trading-and-funding/pricing-and-fees/fees
-      stakingProtocol.fees = 25;
-      break;
-    case 'stakewise':
-      stakingProtocol.id = StakingProtocol.Stakewise;
-      stakingProtocol.name = 'StakeWise';
-      stakingProtocol.logoUrl = 'https://icons.llama.fi/stakewise.png';
-
-      // Source: https://docs.stakewise.io/faq#what-is-the-commission-for-staking-in-stakewise-pool
-      stakingProtocol.fees = 10;
-      break;
-  }
-  protocolMapBySlug.set(slug, stakingProtocol);
-});
+import { getStakingProtocolMapBySlug, getStakingProtocolSlugs } from './staticDataService';
 
 export async function getStakingProtocols(): Promise<StakingProtocolSummary[]> {
   console.log('Fetching staking protocols...');
@@ -85,15 +21,14 @@ export async function getStakingProtocols(): Promise<StakingProtocolSummary[]> {
     throw new Error('Failed to fetch data')
   }
 
+  const protocolMapBySlug = getStakingProtocolMapBySlug();
   response.data.forEach((protocol: any) => {
-    if (protocol.chain === 'Ethereum' && protocolSlugs.includes(protocol.project)) {
+    if (protocol.chain === 'Ethereum' && protocolMapBySlug.has(protocol.project)) {
       const stakingProtocol = protocolMapBySlug.get(protocol.project)!;
       stakingProtocol.tvl = protocol.tvlUsd;
       stakingProtocol.stakingApy = protocol.apyBase;
       stakingProtocol.netApy = protocol.apy;
       stakingProtocol.tokenRewardsApy = protocol.apyReward || 0;
-
-      console.log(protocol.project + ": " + protocol.pool);
       stakingProtocol.defiLlamaPoolId = protocol.pool;
     }
   });
--- app/data/staticDataService.ts
+++ app/data/staticDataService.ts
@@ -1,7 +1,76 @@
-import { StakingProtocol, StakingProtocolDetails } from "../types";
+import { StakingProtocol, StakingProtocolDetails, StakingProtocolSummary } from "../types";
 
 export function getStakingProtocolDetails(protocolId: StakingProtocol): StakingProtocolDetails {
   return protocolDetailsMapById.get(protocolId)!;
+}
+
+// protocols that we are interested in and its DefiLlama project name/slug
+const protocolSlugs = ['lido', 'frax-ether', 'rocket-pool', 'coinbase-wrapped-staked-eth', 'stakewise'];
+const protocolMapBySlug = new Map<string, StakingProtocolSummary>();
+
+// pre-populate the map with supported protocols
+protocolSlugs.forEach((slug: string) => {
+  const stakingProtocol: StakingProtocolSummary = {
+    id: StakingProtocol.Lido,
+    name: '',
+    tvl: 0,
+    netApy: 0,
+    stakingApy: 0,
+    tokenRewardsApy: 0,
+    fees: 0,
+    logoUrl: ''
+  };
+
+  switch (slug) {
+    case 'lido':
+      stakingProtocol.id = StakingProtocol.Lido;
+      stakingProtocol.name = 'Lido';
+      stakingProtocol.logoUrl = 'https://icons.llama.fi/lido.png';
+
+      // Lido takes 10% fee on users staking rewards
+      // Source: https://docs.lido.fi/#protocol-fee
+      stakingProtocol.fees = 10;
+      break;
+    case 'frax-ether':
+      stakingProtocol.id = StakingProtocol.FraxEther;
+      stakingProtocol.name = 'Frax ETH';
+      stakingProtocol.logoUrl = 'https://icons.llama.fi/frax-ether.jpg';
+
+      // Source: https://exponential.fi/assets/b8956e66-fe6c-4a88-8e96-530e2e7dea4c
+      stakingProtocol.fees = 10;
+      break;
+    case 'rocket-pool':
+      stakingProtocol.id = StakingProtocol.RocketPool;
+      stakingProtocol.name = 'Rocket Pool';
+      stakingProtocol.logoUrl = 'https://icons.llama.fi/rocket-pool.jpg';
+
+      // TODO: how do we display 0.05% of deposit fee when ETH is depsoited through protocol?
+      // Source: https://twitter.com/Rocket_Pool/status/1506519957986758659
+      stakingProtocol.fees = 15;
+
+      break;
+    case 'coinbase-wrapped-staked-eth':
+      stakingProtocol.id = StakingProtocol.Coinbase;
+      stakingProtocol.name = 'Coinbase';
+      stakingProtocol.logoUrl = 'https://icons.llama.fi/coinbase-wrapped-staked-eth.png';
+
+      // Source: https://help.coinbase.com/en/coinbase/trading-and-funding/pricing-and-fees/fees
+      stakingProtocol.fees = 25;
+      break;
+    case 'stakewise':
+      stakingProtocol.id = StakingProtocol.Stakewise;
+      stakingProtocol.name = 'StakeWise';
+      stakingProtocol.logoUrl = 'https://icons.llama.fi/stakewise.png';
+
+      // Source: https://docs.stakewise.io/faq#what-is-the-commission-for-staking-in-stakewise-pool
+      stakingProtocol.fees = 10;
+      break;
+  }
+  protocolMapBySlug.set(slug, stakingProtocol);
+});
+
+export function getStakingProtocolMapBySlug(): ReadonlyMap<string, StakingProtocolSummary> {
+  return protocolMapBySlug;
 }
 
 // Static Data Reference: https://docs.google.com/spreadsheets/d/1SEXcufZL1-hskd3bkX8bZPCiuSHwoJe4o-I3xQFLBV8
