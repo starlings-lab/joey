d1d049e3276c123a43abf63aa2e5c19ec4c40a3a
Tai <4382091+taisukemino@users.noreply.github.com>
2023-07-12 20:26:24 -0700

    update the main page contents

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default async function IndexPage() {
   return (
     <main className="p-4 md:p-10 mx-auto max-w-8xl">
       <Flex flexDirection='col' justifyContent="center" alignItems="center">
-        <Title className='text-3xl justify-self-center'>Maximize your ETH</Title>
+        <Title className='text-3xl justify-self-center'>Put your ETH to work</Title>
         <Text className='text-lg'>
-          Discover optimal investment opportunities for your Ethereum with Joey.
+          Discover optimal investment opportunities for your ETH with Joey.
         </Text>
         <Text className='text-lg'>
           We deliver concise comparisons across liquid staking and LSDFi protocols.
