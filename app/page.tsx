2253ee23bded8e779576ea65289da1844c79478d
Tai <4382091+taisukemino@users.noreply.github.com>
2023-07-13 10:58:59 -0700

    align text center in the main

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,8 @@ export default async function IndexPage() {
     <main className="p-4 md:p-10 mx-auto max-w-8xl">
       <Flex flexDirection='col' justifyContent="center" alignItems="center">
         <Title className='text-4xl justify-self-center mb-4'>Put your ETH to work</Title>
-        <Text className='text-lg'>
+        <Text className='text-lg text-center'>
           Discover optimal investment opportunities for your ETH with Joey.
-        </Text>
-        <Text className='text-lg'>
           We deliver concise comparisons across liquid staking and LSDFi protocols.
         </Text>
       </Flex>
