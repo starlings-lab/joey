f69d3e5c31746942a2696eecd6aa0c2473a65978
Tai <4382091+taisukemino@users.noreply.github.com>
2023-07-19 12:04:03 -0700

    import the LSDFiTable component to the main page and inject sample data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import { Card, Title, Text, Flex } from '@tremor/react';
+import { Card, Text, Flex } from '@tremor/react';
 
+import LSDFiTable from './components/LSDFiTable';
 import StakingProtocolsTable from './components/StakingProtocolsTable';
 import { getStakingProtocols } from './data/dataService';
+import { LSDFiStrategy } from './types';
 import { headers } from "next/headers";
 
 export default async function IndexPage() {
@@ -12,6 +14,39 @@ export default async function IndexPage() {
   const endpoint = `http://${host}/api/lsdfi`;
   console.log("Fetching LSDFi strategies from: " + endpoint);
   const lsdfiStrategies = await fetch(endpoint);
+  const lsdFiStrategies = 
+  [
+    {
+      id: LSDFiStrategy.OETH,
+      name: "OETH",
+      tvl: 12,
+      netApy: 6,
+      stakingApy: 0,
+      tokenRewardsApy: 0,
+      fees: 10,
+      features: ["LPing", "Index"]
+    },
+    {
+      id: LSDFiStrategy.AlchemixLidoETH,
+      name: "Alchemix Lido wstETH",
+      tvl: 42,
+      netApy: 5,
+      stakingApy: 0,
+      tokenRewardsApy: 0,
+      fees: 8,
+      features: ["Self-Repaying Loan"]
+    },
+    {
+      id: LSDFiStrategy.SommelierRealYieldETH,
+      name: "Sommelier Real Yield ETH",
+      tvl: 55,
+      netApy: 6,
+      stakingApy: 0,
+      tokenRewardsApy: 0,
+      fees: 5,
+      features: ["Leverage", "LPing"]
+    },
+  ];
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-8xl">
@@ -22,8 +57,13 @@ export default async function IndexPage() {
           We deliver concise comparisons across liquid staking and LSDFi protocols.
         </Text>
       </Flex>
+      <div className='text-xl text-left mt-8 mb-4'>Liquid Staking Protocols</div>
       <Card className="mt-6">
         <StakingProtocolsTable stakingProtocols={stakingProtocols} />
+      </Card>
+      <div className='text-xl text-left mt-8 mb-4'>LSDFi</div>
+      <Card className="mt-6">
+        <LSDFiTable lsdFiStrategies={lsdFiStrategies} />
       </Card>
     </main>
   );
