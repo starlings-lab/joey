e8f476a35d36b45042f4f0174b6d610abca034b8
Tai <4382091+taisukemino@users.noreply.github.com>
2023-07-21 11:02:43 -0700

    style the strategy section

--- app/components/StrategyCard.tsx
+++ app/components/StrategyCard.tsx
@@ -3,19 +3,29 @@
 import { Card, Title } from "@tremor/react";
 import { getLSDFiStrategyHowItWorks, getLSDFiStrategyProtocols } from '../data/staticDataService';
 import { LSDFiStrategy } from '../types';
+import Image from 'next/image';
 
 export default function StrategyCard( {lsdFiStrategy}: {lsdFiStrategy:LSDFiStrategy}) {
   return (
     <Card decoration="top" decorationColor="red">
       <Title className="text-2xl font-bold mb-2">Strategy</Title>
+    <div className="flex mt-2 justify-center">
       <div className="mt-4 md:w-1/2">
-        <p>How It Works</p>
+        <h4 className="text-md font-bold">How It Works</h4>
         <p>{getLSDFiStrategyHowItWorks(lsdFiStrategy)}</p>
       </div>
       <div className="mt-4 md:w-1/2">
-        <p>Protocols</p>
-        <p>{getLSDFiStrategyProtocols(lsdFiStrategy)}</p>
+        <h4 className="text-md font-bold">Protocols</h4>
+        <div>
+          {getLSDFiStrategyProtocols(lsdFiStrategy).map((protocol, index) => (
+            <div className="flex items-center" key={index}>
+              <Image src={`/${protocol}.svg`} width="16" height="16" alt="Protocol" className="w-4 h-4 mr-2" />
+              <p>{protocol}</p>
+            </div>
+          ))}
+        </div>
       </div>
+    </div>
     </Card>
   );
 }
